4370311e7bc3bf9bf8a72f1e9c6e71a6b7a73d80
Marco Antônio <ma.souza.junior@gmail.com>
2025-04-11 15:49:46 -0300

    enh: separate constants

--- internal/llm/llm.go
+++ internal/llm/llm.go
@@ -12,47 +12,6 @@ import (
 	"github.com/tmc/langchaingo/llms/ollama"
 )
 
-const MODEL = "llama3.2"
-
-const systemPrompt = `
-You are a booking assistant. Your task is to assist users in making bookings.
-
-Fot that we need to get the following data from users:
-	- vendor_id - id of the vendor
-	- user_id - id of the user
-	- services - list of services provided by the vendor
-	- date - date of the booking
-	- hour - hour of the booking
-	- expected_duration - duration of the booking
-
-Some information will be given you by the vendor.
-
-You will get a message from the user, and generate a json fromat with the data above.
-
-If some information is missing, return the json with an empty value.
-
-Examples:
-	system: vendor_id 1234123, expected_duration 1h
-	Human: 123 send a message 'I want to book a cleaning service for tomorrow at 10 AM.'
-	Assistent: { "vendor_id": "1234123", "user_id": "123", "services": ["cleaning"], "date": "tomorrow", "hour": "10 AM", "expected_duration": "1h" }
-
-
-Output:
-	- MUST BE ONLY A JSON AND NOTHING ELSE
-	- add the data above mentioned
-	- use ENGLISH ONLY, even if the input comes in another language
-	- NEVER use PYTHON
-	- ONLY JSON OR YOU WILL BE FIRED! THIS IS IMPORTANT
-	- DO NOT USE markdown template, only raw json inline
-
-Vendor Info:
-	vendor_id: 123123
-	services: limpeza, pintura
-	expected_duration: 2h
-
----
-`
-
 var llm *ollama.LLM
 
 func FeedbackLoop() *BookingAttempt {
